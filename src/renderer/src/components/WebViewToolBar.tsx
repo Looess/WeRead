79eb38fc87f8eaef9ccb79e7f73e6a9ba34d27d0
ron.wang <ron.wang@avepoint.com>
2025-04-30 16:50:27 +0800

    feat: refactor WebViewToolBar for improved CSS management and opacity control

--- src/renderer/src/components/WebViewToolBar.tsx
+++ src/renderer/src/components/WebViewToolBar.tsx
@@ -1,77 +1,108 @@
-import React, { useEffect, useState } from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
+import { throttle } from 'lodash';
 import { WebviewTag } from 'electron'
+
+
 interface WebViewToolBarProps {
   webviewRef: React.RefObject<WebviewTag>
 }
+
+
 function WebViewToolBar({ webviewRef }: WebViewToolBarProps): React.JSX.Element {
 
-  const [cssKey, setCssKey] = useState<string | null>(null)
-  const [transparentKey, setTransparent] = useState<string | null>(null)
-  const [opacity, setOpacity] = useState<number>(0.5)
+  const [webViewOpacityKey, setWebViewOpacityKey] = useState<string | null>(null)
+  const [webViewTransparentKey, setWebViewTransparentKey] = useState<string | null>(null)
+  const [opacity, setOpacity] = useState<number>(1)
+
   const [isWebviewReady, setIsWebviewReady] = useState<boolean>(false)
-  const [controlsVisible, setControlsVisible] = useState<boolean>(true)
+  const [toolBarVisible, setToolBarVisible] = useState<boolean>(true)
   const [isPinned, setIsPinned] = useState(false);
 
-  const setBackgroundTransparent = async (): Promise<void> => {
-    if (webviewRef.current && isWebviewReady) {
-      try {
-        const key = await webviewRef.current.insertCSS(`
+
+  const setWebViewBackgroundTransparent = async (): Promise<void> => {
+    if (webviewRef.current) {
+      const key = await webviewRef.current.insertCSS(`
           * {
             background-color: transparent !important;
           }`)
-        setTransparent(key)
-      } catch (error) {
-        console.error('[setBackgroundTransparent] error :', error)
-      }
-    } else {
-      console.warn('WebView 尚未准备好或不可用')
+      setWebViewTransparentKey(key)
     }
   }
-  const removeBackgroundTransparent = async (): Promise<void> => {
-    if (webviewRef.current && isWebviewReady) {
-      try {
-        if (transparentKey) {
-          await webviewRef.current.removeInsertedCSS(transparentKey)
-          setTransparent(null)
-        }
-      } catch (error) {
-        console.error('[removeBackgroundTransparent] error :', error)
+  const setWebViewOpacity = async (): Promise<void> => {
+    if (webviewRef.current) {
+      if (webViewOpacityKey) {
+        await webviewRef.current.removeInsertedCSS(webViewOpacityKey)
       }
-    } else {
-      console.warn('WebView 尚未准备好或不可用')
-    }
-  }
-  // 设置 CSS
-  const setCSS = async (): Promise<void> => {
-    if (webviewRef.current && isWebviewReady) {
-      try {
-        // 如果存在之前注入的 CSS，先移除它
-        if (cssKey) {
-          await webviewRef.current.removeInsertedCSS(cssKey)
-        }
-        const key = await webviewRef.current.insertCSS(`
+      const key = await webviewRef.current.insertCSS(`
           body {
             opacity: ${opacity} !important;
           }
         `)
-        setCssKey(key)
-      } catch (error) {}
-    } else {
+      setWebViewOpacityKey(key)
+    }
+  }
+  const setWebViewCss = async (): Promise<void> => {
+    if (webviewRef.current) {
+      if (!webViewOpacityKey) {
+        await setWebViewOpacity()
+      }
+      if(!webViewTransparentKey) {
+        await setWebViewBackgroundTransparent()
+      }
 
     }
   }
-
-  // 清除 CSS
-  const clearCSS = async (): Promise<void> => {
-    if (webviewRef.current && cssKey && isWebviewReady) {
-      try {
-        await webviewRef.current.removeInsertedCSS(cssKey)
-        setCssKey(null)
-      } catch (error) {}
-    } else {
+  const removeWebViewCss = async (): Promise<void> => {
+    if (webviewRef.current) {
+      if (webViewOpacityKey) {
+        await webviewRef.current.removeInsertedCSS(webViewOpacityKey)
+        setWebViewOpacityKey(null)
+      }
+      if( webViewTransparentKey) {
+        await webviewRef.current.removeInsertedCSS(webViewTransparentKey)
+        setWebViewTransparentKey(null)
+      }
     }
   }
-  const checkPinStatus = async () => {
+
+  useEffect(() => {
+    if (isWebviewReady) {
+      setWebViewOpacity()
+    }
+  }, [opacity])
+
+
+  useEffect(() => {
+    const webview = webviewRef.current
+    if (webview) {
+      const handleReady = (): void => {
+        setIsWebviewReady(true)
+
+      }
+
+      const handleKeyDown = (event: KeyboardEvent): void => {
+        if (event.metaKey && event.key === 'p') {
+          setToolBarVisible((prev) => !prev);
+        }
+      }
+      setPinStatus()
+      window.addEventListener('keydown', handleKeyDown)
+      webview.addEventListener('dom-ready', handleReady)
+      return (): void => {
+        webview.removeEventListener('dom-ready', handleReady)
+        window.removeEventListener('keydown', handleKeyDown)
+      }
+    }
+    return (): void => {}
+  }, [])
+  const throttledChangeHandler = useCallback(
+      throttle((value: number) => {
+        setOpacity(value);
+      }, 100),
+      []
+  );
+
+  const setPinStatus = async () => {
     const status = await window.context.getPinStatus();
     setIsPinned(status);
   };
@@ -79,80 +110,41 @@ function WebViewToolBar({ webviewRef }: WebViewToolBarProps): React.JSX.Element 
     const status = await window.context.togglePinToDesktop(!isPinned);
     setIsPinned(status);
   };
-  useEffect(() => {
-    // 监听 webview dom-ready 事件
-    const webview = webviewRef.current
-    if (webview) {
-      const handleReady = (): void => {
-        console.log('WebView DOM 已准备好')
-        setIsWebviewReady(true)
-      }
-      checkPinStatus();
-      webview.addEventListener('dom-ready', handleReady)
-      const handleKeyDown = (event: KeyboardEvent): void => {
-        // 检查组合键 (Ctrl+H)
-        if (event.metaKey && event.key === 'p') {
-          toggleControls() // 切换控制面板可见性
-        }
-      }
-      window.addEventListener('keydown', handleKeyDown)
-      return (): void => {
-        webview.removeEventListener('dom-ready', handleReady)
-        window.removeEventListener('keydown', handleKeyDown)
-      }
-    }
 
-    // 当 webview 不存在时返回空函数作为清理函数
-    return (): void => {}
-    //空数组代表只在组件挂载时执行
-  }, [])
 
-  // 当 opacity 变化时自动应用 CSS
-  useEffect(() => {
-    if (isWebviewReady) {
-      setCSS()
-    }
-  }, [opacity])
-
-  const handleOpacityChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setOpacity(parseFloat(event.target.value))
-  }
-  const toggleControls = (): void => {
-    setControlsVisible((prev) => !prev)
-  }
   return (
-    // <div className="webview-container">
-    //   <div className="controls">
       <div>
-        {controlsVisible && (
+        {toolBarVisible && (
             <div className="controls">
-              <button onClick={setBackgroundTransparent}>Set Transparent</button>
-              <button onClick={removeBackgroundTransparent}>
-                Remove Transparent
-              </button>
-              <button onClick={setCSS}>Set CSS</button>
-              <button onClick={clearCSS}>Clear CSS</button>
+              <button onClick={setWebViewCss}>🌫️</button>
+              <button onClick={removeWebViewCss}>❌</button>
+              {
+                  webViewOpacityKey && (
+                      <input
+                          type="range"
+                          min="0"
+                          max="1"
+                          step="0.05"
+                          value={opacity}
+                          onChange={(e) => throttledChangeHandler(parseFloat(e.target.value))}
+
+                      />
+                  )
+              }
+
               <button
                   onClick={togglePin}
                   title={isPinned ? "Unpin from desktop" : "Pin to desktop"}
               >
                 {isPinned ? "📌" : "📍"}
               </button>
-              <input
-                  type="range"
-                  min="0"
-                  max="1"
-                  step="0.05"
-                  value={opacity}
-                  onChange={handleOpacityChange}
-              />
+
             </div>
         )}
       </div>
 
-    //   </div>
-    // </div>
   )
+
 }
 
 export default WebViewToolBar
